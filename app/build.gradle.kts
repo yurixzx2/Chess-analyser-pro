3c2dc35ca965dc8ab47f410208561b3c6add7cc4
yurixzx2 <yurixzx2@users.noreply.github.com>
2026-07-08 16:32:49 +0000

    fix: correct TensorFlow Lite dependency (add base tensorflow-lite package)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,9 +65,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // TensorFlow Lite for Maia Engine
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.0")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.10.0")
-    implementation("org.tensorflow:tensorflow-lite-select-tf-ops:2.10.0")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.14.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
